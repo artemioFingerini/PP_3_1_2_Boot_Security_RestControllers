3f02ea971e0e03d0d3310768957608c96c22d5f0
artemioFingerini <artemisanx@gmail.com>
2024-10-11 12:30:17 +0300

    commitBeforeReview1

--- src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
@@ -2,48 +2,53 @@ package ru.kata.spring.boot_security.demo.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import ru.kata.spring.boot_security.demo.services.UserServiceImpl;
 
 @Configuration
 @EnableWebSecurity
+@EnableJpaRepositories(basePackages = "ru.kata.spring.boot_security.demo")
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    private final SuccessUserHandler successUserHandler;
 
-    public WebSecurityConfig(SuccessUserHandler successUserHandler) {
+    private final SuccessUserHandler successUserHandler;
+    private final UserServiceImpl userService;
+
+    public WebSecurityConfig(SuccessUserHandler successUserHandler, UserServiceImpl userService) {
         this.successUserHandler = successUserHandler;
+        this.userService = userService;
     }
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/index").permitAll()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/user/**").hasAnyRole("USER", "ADMIN")
                 .anyRequest().authenticated()
                 .and()
-                .formLogin().successHandler(successUserHandler)
+                .formLogin()
+                .successHandler(successUserHandler)
                 .permitAll()
                 .and()
                 .logout()
                 .permitAll();
     }
 
-    // аутентификация inMemory
-    @Bean
     @Override
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("user")
-                        .password("user")
-                        .roles("USER")
-                        .build();
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService)
+                .passwordEncoder(passwordEncoder());
+    }
 
-        return new InMemoryUserDetailsManager(user);
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
---
+++ src/main/java/ru/kata/spring/boot_security/demo/services/UserServiceImpl.java
@@ -0,0 +1,81 @@
+package ru.kata.spring.boot_security.demo.services;
+
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.kata.spring.boot_security.demo.entities.Role;
+import ru.kata.spring.boot_security.demo.entities.User;
+import ru.kata.spring.boot_security.demo.repositories.RoleRepository;
+import ru.kata.spring.boot_security.demo.repositories.UserRepository;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+
+
+@Service
+public class UserServiceImpl implements UserDetailsService {
+
+    private final UserRepository usersRepository;
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+
+
+    public UserServiceImpl(UserRepository usersRepository, RoleRepository roleRepository, @Lazy PasswordEncoder passwordEncoder) {
+        this.usersRepository = usersRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+
+    @Transactional(readOnly = true)
+    public List<User> getAllUsers() {
+        return usersRepository.findAll();
+    }
+
+
+    @Transactional(readOnly = true)
+    public User getById(Long id) {
+        return usersRepository.getById(id);
+    }
+
+
+    @Transactional
+    public void saveUser(@Valid User user, List<Long> roleIds) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        for (Long roleId : roleIds) {
+            Optional<Role> role = roleRepository.findById(roleId);
+            user.addRole(role.orElse(null));
+        }
+        usersRepository.save(user);
+    }
+
+
+    @Transactional
+    public void deleteUserById(Long id) {
+        User user = usersRepository.getById(id);
+        usersRepository.delete(user);
+    }
+
+    @Transactional
+    public void updateUser(@Valid User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        usersRepository.save(user);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Role> findAll() {
+        return roleRepository.findAll();
+    }
+
+    @Transactional
+    @Override
+    public UserDetails loadUserByUsername(String username) {
+        User user = usersRepository.findByUsername(username);
+        return user;
+    }
+}
+
